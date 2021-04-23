c7696a0686fcda3c4c71cd9a0da666d16f96fd3d
noctillion <ornatus30@gmail.com>
2021-04-22 20:56:23 -0400

    test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// redux
+import { createStore } from 'redux'
+import rootReducer from './reducers/index'
+import {Provider} from 'react-redux'
 
+const store = createStore(rootReducer)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  
   document.getElementById('root')
 );
